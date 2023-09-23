457a3f9997a14b17529563c947818321695dabf2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-09-23 15:28:49 +0900

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by joe on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
