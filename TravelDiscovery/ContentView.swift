a0164ea1a79fec7459e32f47a594285603375f3a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-09-24 00:17:57 +0900

    ForEach Hashables and system images

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -10,7 +10,106 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            DiscoverCategoriesView()
+            ScrollView {
+                DiscoverCategoriesView()
+                PopularDestinationsView()
+                PopularRestaurantsView()
+                TrendingCreatorsView()
+            }
+            .navigationTitle("Discover")
+            // .navigationBarTitleDisplayMode(.inline)
+            // .toolbarBackground(Color.clear, for: .navigationBar)
+            // .toolbarBackground(.visible, for: .navigationBar)
+        }
+    }
+}
+
+struct PopularDestinationsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular destinations")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5) { num in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5) { num in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending Creators")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    ForEach(0..<15) { num in
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }
+                .padding(.horizontal)
+            }
         }
     }
 }
@@ -21,29 +120,40 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct Category: Hashable {
+    let name, imageName: String
+}
+
 struct DiscoverCategoriesView: View {
+    let categories: [Category] = [
+        Category(name: "Art", imageName: "paintpalette.fill"),
+        Category(name: "Sport", imageName: "sportscourt.fill"),
+        Category(name: "Live Events", imageName: "music.mic"),
+        Category(name: "Food", imageName: "fork.knife.circle.fill"),
+        Category(name: "History", imageName: "books.vertical.fill")
+    ]
+    
     var body: some View {
-        ScrollView {
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 8) {
-                    ForEach(0..<5, id: \.self) { num in
-                        VStack(spacing: 4) {
-                            Spacer()
-                                .frame(width: 50, height: 50)
-                                .background(Color.gray)
-                                .cornerRadius(.infinity)
-                                .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
-                            Text("Art")
-                                .font(.system(size: 12, weight: .semibold))
-                        }
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 14) {
+                ForEach(categories, id: \.self) { category in
+                    VStack(spacing: 8) {
+//                        Spacer()
+                        Image(systemName: category.imageName)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .frame(width: 64, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text(category.name)
+                            .font(.system(size: 12, weight: .semibold))
+                            .multilineTextAlignment(.center)
                     }
+                    .frame(width: 68)
                 }
-                .padding()
             }
+            .padding(.horizontal)
         }
-        .navigationTitle("Discover")
-        // .navigationBarTitleDisplayMode(.inline)
-        // .toolbarBackground(Color.clear, for: .navigationBar)
-        // .toolbarBackground(.visible, for: .navigationBar)
     }
 }
