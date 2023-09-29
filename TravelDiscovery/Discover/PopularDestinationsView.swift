621ac7320b7d5102e36e240d6f7b603fdb9c01f8
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-09-29 19:28:49 +0900

    SwiftUI Map views using latitude and longitude

--- TravelDiscovery/Discover/PopularDestinationsView.swift
+++ TravelDiscovery/Discover/PopularDestinationsView.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PopularDestinationsView: View {
     let destinations = [
-        Destination(name: "Paris", country: "France", imageName: "eiffel_tower"),
-        Destination(name: "Tokyo", country: "Japan", imageName: "japan"),
-        Destination(name: "New York", country: "USA", imageName: "new_york")
+        Destination(name: "Paris", country: "France", imageName: "eiffel_tower", latitude: 48.855014, longitude: 2.341231),
+        Destination(name: "Tokyo", country: "Japan", imageName: "japan", latitude: 35.67988, longitude: 139.7695),
+        Destination(name: "New York", country: "USA", imageName: "new_york", latitude: 40.71592, longitude: -74.0055)
     ]
     
     var body: some View {
@@ -47,8 +48,12 @@ struct PopularDestinationsView: View {
 struct PopularDestinationDetailsView: View {
     let destination: Destination
     
+//    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.859565, longitude: 2.353235), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    @State private var region: MKCoordinateRegion
+    
     init(destination: Destination) {
         self.destination = destination
+        self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: destination.latitude, longitude: destination.longitude), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
         navigationBarInitialization()
     }
     
@@ -57,10 +62,12 @@ struct PopularDestinationDetailsView: View {
             Image(destination.imageName)
                 .resizable()
                 .scaledToFill()
+            
             VStack(alignment: .leading) {
                 Text(destination.name)
-                    .font(.system(size: 18, weight: .bold))
+                    .font(.system(size: 19, weight: .bold))
                 Text(destination.country)
+                    .padding(.bottom, 5)
                 
                 HStack {
                     ForEach(0..<5, id: \.self) { num in
@@ -68,14 +75,24 @@ struct PopularDestinationDetailsView: View {
                             .foregroundColor(.orange)
                     }
                 }
-                .padding(.top, 1)
                 
                 Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
                     .padding(.top, 4)
+                    .font(.system(size: 17))
                 
                 HStack { Spacer() }
             }
             .padding(.horizontal)
+            
+            HStack {
+                Text("Location")
+                    .font(.system(size: 19, weight: .semibold))
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Map(coordinateRegion: $region)
+                .frame(height: 200)
         }
         .navigationTitle(destination.name)
         .navigationBarTitleDisplayMode(.inline)
@@ -115,7 +132,8 @@ struct PopularDestinationTile: View {
 struct PopularDestinationsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            PopularDestinationDetailsView(destination: Destination(name: "Paris", country: "France", imageName: "eiffel_tower"))
+//            PopularDestinationDetailsView(destination: Destination(name: "Paris", country: "France", imageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235))
+            PopularDestinationDetailsView(destination: Destination(name: "Tokyo", country: "Japan", imageName: "japan", latitude: 35.679693, longitude: 139.771913))
         }
         DiscoverView()
         PopularDestinationsView()
