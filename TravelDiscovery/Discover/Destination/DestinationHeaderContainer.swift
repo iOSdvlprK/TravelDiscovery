c63be948c9b3eed1a6ae6b84201a3f31b83740ea
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-10-02 23:02:11 +0900

    integrate swiping with UIPageViewController and UIHostingController

--- TravelDiscovery/Discover/Destination/DestinationHeaderContainer.swift
+++ TravelDiscovery/Discover/Destination/DestinationHeaderContainer.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct DestinationHeaderContainer: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        let redVC = UIViewController()
-        redVC.view.backgroundColor = .systemRed
-        return redVC
+//        let redVC = UIHostingController(rootView: Text("First View Controller"))
+//        return redVC
+        let pvc = CustomPageViewController()
+        return pvc
     }
     
     typealias UIViewControllerType = UIViewController
@@ -20,8 +21,57 @@ struct DestinationHeaderContainer: UIViewControllerRepresentable {
     }
 }
 
+class CustomPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        allControllers.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        0
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = allControllers.firstIndex(of: viewController) else { return nil }
+        if index == 0 { return nil }
+        return allControllers[index - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = allControllers.firstIndex(of: viewController) else { return nil }
+        if index == allControllers.count - 1 { return nil }
+        return allControllers[index + 1]
+    }
+    
+    let firstVC = UIHostingController(rootView: Text("First View Controller"))
+    let secondVC = UIHostingController(rootView: Text("Second View Controller"))
+    let thirdVC = UIHostingController(rootView: Text("Third View Controller"))
+    
+    lazy var allControllers: [UIViewController] = [
+        firstVC, secondVC, thirdVC
+    ]
+    
+    init() {
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.systemGray5
+        UIPageControl.appearance().currentPageIndicatorTintColor = .blue
+        
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        
+        setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        
+        self.dataSource = self
+        self.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 struct DestinationHeaderContainer_Previews: PreviewProvider {
     static var previews: some View {
         DestinationHeaderContainer()
+        NavigationStack {
+            PopularDestinationDetailsView(destination: Destination(name: "Paris", country: "France", imageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235))
+        }
     }
 }
