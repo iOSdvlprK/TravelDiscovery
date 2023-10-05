e80f867396bca7c8c315ada37b07274745a2f61b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-10-05 23:10:24 +0900

    fetch popular dishes from JSON in RestaurantDetailsView

--- TravelDiscovery/Discover/Restaurant/RestaurantDetailsView.swift
+++ TravelDiscovery/Discover/Restaurant/RestaurantDetailsView.swift
@@ -6,8 +6,42 @@
 //
 
 import SwiftUI
+import Kingfisher
+
+struct RestaurantDetails: Decodable {
+    let description: String
+    let popularDishes: [Dish]
+}
+
+struct Dish: Decodable, Hashable {
+    let name, price, photo: String
+    let numPhotos: Int
+}
+
+class RestaurantDetailsViewModel: ObservableObject {
+    @Published var isLoading = true
+    @Published var details: RestaurantDetails?
+    
+    init() {
+        let urlString = "https://travel.letsbuildthatapp.com/travel_discovery/restaurant?id=0"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, resp, err in
+            // handle the errors properly
+            
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async {
+                self.details = try? JSONDecoder().decode(RestaurantDetails.self, from: data)
+            }
+        }.resume()
+    }
+}
 
 struct RestaurantDetailsView: View {
+    @ObservedObject var vm = RestaurantDetailsViewModel()
+    
     let restaurant: Restaurant
     
     init(restaurant: Restaurant) {
@@ -61,7 +95,7 @@ struct RestaurantDetailsView: View {
                     .foregroundColor(.orange)
                 }
                 
-                Text("Usually when you want to write a very long description, you want to make sure that it spans at least a few lines. When I'm testing I like to see at least 5 lines of text so that things are auto sized correctly. One more line of text should be better just to be safe.")
+                Text(vm.details?.description ?? "")
                     .padding(.top, 8)
                     .font(.system(size: 15, weight: .regular))
             }
@@ -76,22 +110,8 @@ struct RestaurantDetailsView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 16) {
-                    ForEach(0..<5) { num in
-                        VStack(alignment: .leading) {
-                            Image("tapas")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(height: 80)
-                                .cornerRadius(5)
-                                .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.gray))
-                                .shadow(radius: 2)
-                                .padding(.vertical, 2)
-                            Text("Japanese Tapas")
-                                .font(.system(size: 15, weight: .bold))
-                            Text("88 photos")
-                                .foregroundColor(.gray)
-                                .font(.system(size: 13, weight: .regular))
-                        }
+                    ForEach(vm.details?.popularDishes ?? [], id: \.self) { dish in
+                        DishCell(dish: dish)
                     }
                 }
                 .padding(.horizontal)
@@ -100,6 +120,46 @@ struct RestaurantDetailsView: View {
         .navigationTitle("Restaurant Details")
         .navigationBarTitleDisplayMode(.inline)
         .navBarInit()
+    }
+    
+    /*
+    let sampleDishPhotos = [
+        "https://letsbuildthatapp-videos.s3.us-west-2.amazonaws.com/0d1d2e79-2f10-4cfd-82da-a1c2ab3638d2",
+        "https://letsbuildthatapp-videos.s3.us-west-2.amazonaws.com/3a352f87-3dc1-4fa7-affe-fb12fa8691fe"
+    ]
+    */
+}
+
+struct DishCell: View {
+    let dish: Dish
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ZStack(alignment: .bottomLeading) {
+                KFImage(URL(string: dish.photo))
+                    .resizable()
+                    .scaledToFill()
+                    .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.gray))
+                    .shadow(radius: 2)
+                    .padding(.vertical, 2)
+                
+                LinearGradient(colors: [.clear, .black], startPoint: .center, endPoint: .bottom)
+                
+                Text(dish.price)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14, weight: .bold))
+                    .padding(.horizontal, 6)
+                    .padding(.bottom, 4)
+            }
+            .frame(height: 120)
+            .cornerRadius(5)
+            
+            Text(dish.name)
+                .font(.system(size: 15, weight: .bold))
+            Text("\(dish.numPhotos) photos")
+                .foregroundColor(.gray)
+                .font(.system(size: 13, weight: .regular))
+        }
     }
 }
 
